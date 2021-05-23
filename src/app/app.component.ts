b8dc6759965ff2df4b7fdffa7ec3bf9876ad3845
Naris Binwaearong <ris@MacBook-Pro-khxng-Naris.local>
2021-05-23 17:04:45 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeeapp';
+export class AppComponent implements OnInit {
+ public employees : Employee[];
+ public editEmployee:Employee;
+  public deleteEmployee: Employee;
+ constructor(private employeeservice :EmployeeService){}
+
+ ngOnInit(){
+   this.getemployee();
+ }
+
+ public getemployee():void{
+   this.employeeservice.getEmployee().subscribe(
+     (response:Employee[])=>{
+       this.employees=response;
+     },
+     (error:HttpErrorResponse)=>{
+       alert(error.message);
+     }
+   );
+ }
+
+ public onAddEmployee(addForm:NgForm):void{
+   document.getElementById('add-employee-form').click();
+   this.employeeservice.addEmployee(addForm.value).subscribe(
+     (response:Employee)=>{
+       console.log(response);
+       this.getemployee();
+       addForm.reset();
+     },
+     (error:HttpErrorResponse)=>{
+       alert(error.message);
+     }
+   );
+ }
+
+
+ public onUpdateEmployee(employee:Employee):void{
+
+  this.employeeservice.updateEmployee(employee).subscribe(
+    (response:Employee)=>{
+      console.log(response);
+      this.getemployee();
+    },
+    (error:HttpErrorResponse)=>{
+      alert(error.message);
+    }
+  );
+}
+
+public onDeleteEmployee(employeeId:number):void{
+
+  this.employeeservice.deleteEmployee(employeeId).subscribe(
+    (response:void)=>{
+      console.log(response);
+      this.getemployee();
+    },
+    (error:HttpErrorResponse)=>{
+      alert(error.message);
+    }
+  );
+}
+
+
+ public onOpenModal(employee:Employee,mode:string):void{
+   const container = document.getElementById('main-container')
+   const button = document.createElement('button');
+   button.type='button';
+   button.style.display='none';
+   button.setAttribute('data-toggle','modal')
+   if(mode ==='add'){
+     button.setAttribute('data-target','#addEmployeeModal');
+   }
+   if(mode ==='edit'){
+     this.editEmployee= employee;
+     button.setAttribute('data-target','#updataEmployeeModal');
+   }
+   if(mode ==='delete'){
+     this.deleteEmployee=employee;
+     button.setAttribute('data-target','#deleteEmployeeModal')
+   }
+   container.appendChild(button);
+   button.click();
+ }
+
+
+
+
+
 }
